f3c7f00d4746ab87e0a99f1b1811cbb92d06c577
Adam Aldoreh <37591733+Aldoreh46@users.noreply.github.com>
2021-09-19 13:55:03 -0400

    Added Files

--- Solution.java
+++ Solution.java
@@ -1,32 +1,11 @@
 package JavaRevisions;
 
+import java.io.*;
 import java.util.*;
-import java.text.*;
 
 public class Solution {
-    
-  //  public Locale(String language,String country){
- //    return       
-  //  }
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        double payment = scanner.nextDouble();
-        scanner.close();
-        NumberFormat n = NumberFormat.getCurrencyInstance(Locale.US);
-        String us = n.format(payment);
-       NumberFormat n1 = NumberFormat.getCurrencyInstance(new Locale("en", "IN"));
-        String india = n1.format(payment);
-        NumberFormat n2 = NumberFormat.getCurrencyInstance(Locale.CHINA);
-        String china = n2.format(payment);
-        NumberFormat n3 = NumberFormat.getCurrencyInstance(Locale.FRANCE);
-        String france = n3.format(payment);
-        
-        // Write your code here.
-        
-        System.out.println("US: " + us);
-        System.out.println("India: " + india);
-        System.out.println("China: " + china);
-        System.out.println("France: " + france);
 
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
     }
 }
