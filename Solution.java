8a4d05ba75106bb54ccb00ca25f07125d11d56b6
Adam Aldoreh <37591733+Aldoreh46@users.noreply.github.com>
2021-10-03 22:01:56 -0400

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,39 +1,36 @@
 package JavaRevisions;
 
-import java.io.*;
 import java.util.*;
-
 
 public class Solution {
 
-  public static void main(String[] args) {
-      Scanner scan = new Scanner(System.in);
-      int n = scan.nextInt();
-      int[] a = new int[n];
-      
-      for(int i = 0;i < n;i++){
-          a[i] = scan.nextInt();
-      }
-      scan.close();
-      
-      int numNegativeSubarrays = 0;
-      
-      for(int i = 0; i < n; i++) {
-          for(int j = i; j < n; j++) {
-              
-              int sum = 0;
-              
-              for(int k = i; k <= j; k++) {
-                  sum = sum + a[k];
-              }
-              
-              if(sum < 0) {
-                  numNegativeSubarrays++;
-              }
-          }
-      }
-      
-      System.out.println(numNegativeSubarrays);
-  }
+    public static boolean canWin(int leap, int[] game) {
+        return isSolvable(leap, game, 0);
+    }
+
+    private static boolean isSolvable(int m, int[] arr, int i) {
+        if (i < 0 || arr[i] == 1) return false;
+        if ((i == arr.length - 1) || i + m > arr.length - 1) return true;
+
+        arr[i] = 1;
+        return isSolvable(m, arr, i + 1) || isSolvable(m, arr, i - 1) || isSolvable(m, arr, i + m);
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int q = scan.nextInt();
+        while (q-- > 0) {
+            int n = scan.nextInt();
+            int leap = scan.nextInt();
+            
+            int[] game = new int[n];
+            for (int i = 0; i < n; i++) {
+                game[i] = scan.nextInt();
+            }
+
+            System.out.println( (canWin(leap, game)) ? "YES" : "NO" );
+        }
+        scan.close();
+    }
 }
-    
